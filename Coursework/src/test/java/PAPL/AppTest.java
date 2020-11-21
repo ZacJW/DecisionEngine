8a6f22a4f63edbfaaf5f100567a0f15edc508033
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-11-21 13:46:19 +0000

    Initial Commit

---
+++ Coursework/src/test/java/PAPL/AppTest.java
@@ -0,0 +1,20 @@
+package PAPL;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

4c73a3f965f4497ac1807d24a9f2a7dbbe1bbfa8
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-12-02 10:42:19 +0000

    Updated event capture interface

--- Coursework/src/main/java/DecisionEngine/Core/EventCaptureInterface.java
+++ Coursework/src/main/java/DecisionEngine/Core/EventCaptureInterface.java
@@ -1,11 +1,13 @@
 package DecisionEngine.Core;
 
-import java.util.Set;
+import java.util.Collection;
+import java.util.Iterator;
 
 import DecisionEngine.Event.GameEventInterface;
 
-public interface EventCaptureInterface {
-    public Set<GameEventInterface> getEventCheckSet();
-    public void resetEventCheckSet();
-    public void registerEventChecks(Set<GameEventInterface> events);
+public interface EventCaptureInterface extends Iterable<GameEventInterface> {
+    public void add(GameEventInterface event);
+    public void add(Collection<GameEventInterface> events);
+    public Iterator<GameEventInterface> iterator();
+    void reset();
 }

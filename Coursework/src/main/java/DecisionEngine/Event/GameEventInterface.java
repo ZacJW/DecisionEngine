1fff3db61c75b91503ec0b27d3c448ee63b62478
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-12-02 10:37:40 +0000

    Changed game events to be runnable

--- Coursework/src/main/java/DecisionEngine/Event/GameEventInterface.java
+++ Coursework/src/main/java/DecisionEngine/Event/GameEventInterface.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 
 import DecisionEngine.GameObject.StateLinkInterface;
 
-public interface GameEventInterface {
+public interface GameEventInterface extends Runnable {
     public Collection<? extends StateLinkInterface> getLinks();
     public Collection<? extends GameEventInterface> getListeners();
     public boolean check();

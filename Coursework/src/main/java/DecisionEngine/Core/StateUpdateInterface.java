7965f50668a233a704863dab9b3cbb1d86d75df2
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-12-03 12:49:37 +0000

    Added javadoc to StateUpdateInterface

--- Coursework/src/main/java/DecisionEngine/Core/StateUpdateInterface.java
+++ Coursework/src/main/java/DecisionEngine/Core/StateUpdateInterface.java
@@ -5,8 +5,21 @@ import java.util.Iterator;
 import DecisionEngine.GameObject.StateLinkInterface;
 import DecisionEngine.GameObject.StateNodeInterface;
 
+/**
+ * The interface for capturing state changes in StateMapInterface objects
+ */
 public interface StateUpdateInterface extends Iterable<StateNodeInterface> {
+    /**
+     * Captures a state node and state link to be updated at the next state update step
+     * If node is already captured, the priority of the existing link and new link are
+     * compared, and the higer priority link is kept in the capture.
+     * @param node The currently active StateNodeInterface with a triggered link
+     * @param link The triggered StateLinkInterface from node
+     */
     public void add(StateNodeInterface node, StateLinkInterface link);
     public Iterator<StateNodeInterface> iterator();
+    /**
+     * Empties the state update capture
+     */
     public void reset();
 }

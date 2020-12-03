69304fbd789b839b044ff8c73bec4135558b0280
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-12-03 12:39:40 +0000

    Added javadoc to EventCaptureInterface

--- Coursework/src/main/java/DecisionEngine/Core/EventCaptureInterface.java
+++ Coursework/src/main/java/DecisionEngine/Core/EventCaptureInterface.java
@@ -5,9 +5,24 @@ import java.util.Iterator;
 
 import DecisionEngine.Event.GameEventInterface;
 
+/**
+ * The interface for capturing game events that need to be re-checked when their listened variables change.
+ * Must be thread-safe
+ */
 public interface EventCaptureInterface extends Iterable<GameEventInterface> {
+    /**
+     * Captures an event to be re-processed later
+     * @param event the event to be re-processed at the next event step
+     */
     public void add(GameEventInterface event);
+    /**
+     * Captures many events to be re-processed later
+     * @param events a Collection of events to be re-processed at the next event step
+     */
     public void add(Collection<GameEventInterface> events);
     public Iterator<GameEventInterface> iterator();
+    /**
+     * Empties the event capture
+     */
     void reset();
 }

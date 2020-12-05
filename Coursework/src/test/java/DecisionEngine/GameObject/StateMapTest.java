42642e22694d0009fb0dff47dfdb3ce3a6c9d7a0
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-12-05 15:18:28 +0000

    Added StateMapTest

---
+++ Coursework/src/test/java/DecisionEngine/GameObject/StateMapTest.java
@@ -0,0 +1,39 @@
+package DecisionEngine.GameObject;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class StateMapTest {
+    @Test
+    public void activeNodeTest() throws NullPointerException{
+        StateMap map = new StateMap();
+        StateNode node1 = new Node(map);
+        map.pushNode(node1);
+        StateNode node2 = new Node(map);
+        map.pushNode(node2);
+        map.setActiveNode(node1);
+        assertEquals(map.getActiveNode(), node1);
+        assertNotEquals(map.getActiveNode(), node2);
+        assertTrue(node1.isActive());
+        assertFalse(node2.isActive());
+        map.setActiveNode(node2);
+        assertEquals(map.getActiveNode(), node2);
+        assertNotEquals(map.getActiveNode(), node1);
+        assertTrue(node2.isActive());
+        assertFalse(node1.isActive());
+    }
+}
+
+class Node extends StateNode{
+    public Node(StateMapInterface map) throws NullPointerException {
+        super(map);
+    }
+
+    public void behaviour() {
+
+    }
+}

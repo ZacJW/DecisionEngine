0e31647df9e306bdae56422bc898c6df84b63247
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-12-12 14:40:18 +0000

    Wrote GameEvent abstract class

---
+++ Coursework/src/main/java/DecisionEngine/Event/GameEvent.java
@@ -0,0 +1,51 @@
+package DecisionEngine.Event;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import DecisionEngine.Core.World;
+import DecisionEngine.GameObject.StateLinkInterface;
+
+public abstract class GameEvent implements GameEventInterface {
+    Set<StateLinkInterface> links = new HashSet<StateLinkInterface>();
+    Set<GameEventInterface> listeners = new HashSet<GameEventInterface>();
+    World world;
+
+    public GameEvent(World world){
+        this.world = world;
+    }
+    
+    public void run() {
+        for (StateLinkInterface link : links){
+            world.addStateUpdate(link.getFrom(), link);
+        }
+
+    }
+
+    public Collection<? extends StateLinkInterface> getLinks() {
+        return links;
+    }
+
+    public void addLink(StateLinkInterface link){
+        links.add(link);
+    }
+
+    public void removeLink(StateLinkInterface link) {
+        links.remove(link);
+
+    }
+
+    public Collection<? extends GameEventInterface> getListeners() {
+        return listeners;
+    }
+
+    public abstract boolean check();
+
+    public void trigger() {
+        for (StateLinkInterface link : links){
+            world.addStateUpdate(link.getFrom(), link);
+        }
+    }
+    
+}

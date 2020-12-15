14129b0c727cc2fecc235a1fecceea037c70eae2
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-12-15 10:11:16 +0000

    Updated GameObject for updated interface

--- Coursework/src/main/java/DecisionEngine/GameObject/GameObject.java
+++ Coursework/src/main/java/DecisionEngine/GameObject/GameObject.java
@@ -7,17 +7,28 @@ import DecisionEngine.Core.World;
 public class GameObject implements GameObjectInterface {
     ArrayList<StateMapInterface> maps;
     World world;
+    GameObjectInterface parent;
+    ArrayList<GameObjectInterface> children;
 
     public GameObject() {
-        
+
     }
 
-    public ArrayList<StateMapInterface> getStateMaps(){
+    public ArrayList<StateMapInterface> getStateMaps() {
         return maps;
     }
-    public void run(){
-        for (StateMapInterface map : maps){
+
+    public void run() {
+        for (StateMapInterface map : maps) {
             map.getActiveNode().behaviour();
         }
+    }
+
+    public ArrayList<GameObjectInterface> getChildren() {
+        return children;
+    }
+
+    public GameObjectInterface getParent() {
+        return parent;
     }
 }

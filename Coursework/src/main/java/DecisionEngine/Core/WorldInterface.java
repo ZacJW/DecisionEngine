87adb8e669dc08c64f349bf5ceb3a9e359cfd2dc
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-12-15 10:07:21 +0000

    Created WorldInterface

---
+++ Coursework/src/main/java/DecisionEngine/Core/WorldInterface.java
@@ -0,0 +1,11 @@
+package DecisionEngine.Core;
+
+public interface WorldInterface {
+    public void mainloop();
+
+    public void processBehaviours();
+
+    public void processEvents();
+
+    public void updateStates();
+}

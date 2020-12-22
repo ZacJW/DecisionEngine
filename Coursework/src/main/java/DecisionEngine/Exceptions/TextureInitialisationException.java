d05e2c058c4a3cefaefcab92afb1fa5ef945d79e
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-12-22 22:18:57 +0000

    Changed TextureInitialisationException serial ID

--- Coursework/src/main/java/DecisionEngine/Exceptions/TextureInitialisationException.java
+++ Coursework/src/main/java/DecisionEngine/Exceptions/TextureInitialisationException.java
@@ -5,8 +5,9 @@ public class TextureInitialisationException extends Exception {
     /**
      *
      */
-    private static final long serialVersionUID = 1L;
-    public TextureInitialisationException(String message){
+    private static final long serialVersionUID = 7690716630932806383L;
+
+    public TextureInitialisationException(String message) {
         super(message);
     }
 }

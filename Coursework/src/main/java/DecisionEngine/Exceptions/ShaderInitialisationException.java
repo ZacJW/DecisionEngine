82b16e074e1d8d85b270ef9375a50acc892ed3c7
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-12-22 22:19:19 +0000

    Created ShaderInitialisationException

---
+++ Coursework/src/main/java/DecisionEngine/Exceptions/ShaderInitialisationException.java
@@ -0,0 +1,12 @@
+package DecisionEngine.Exceptions;
+
+public class ShaderInitialisationException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -2676091875295811551L;
+    public ShaderInitialisationException(String message){
+        super(message);
+    }
+}

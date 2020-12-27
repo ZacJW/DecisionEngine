ca4f1e728227592c39c27cdc94689c770e3f9cac
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-12-27 20:03:59 +0000

    Added point 2d

---
+++ Coursework/src/main/java/DecisionEngine/Utils/Point2D_Double.java
@@ -0,0 +1,10 @@
+package DecisionEngine.Utils;
+
+public class Point2D_Double {
+    double x;
+    double y;
+    public Point2D_Double(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+}

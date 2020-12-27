b7221237cf6f3c6f8175d0ea3577335216764d71
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-12-27 13:51:47 +0000

    Removed parent and children from game objects

--- Coursework/src/main/java/DecisionEngine/GameObject/GameObject.java
+++ Coursework/src/main/java/DecisionEngine/GameObject/GameObject.java
@@ -7,8 +7,6 @@ import DecisionEngine.Core.World;
 public class GameObject implements GameObjectInterface {
     ArrayList<StateMapInterface> maps;
     World world;
-    GameObjectInterface parent;
-    ArrayList<GameObjectInterface> children;
 
     public GameObject() {
 
@@ -22,13 +20,5 @@ public class GameObject implements GameObjectInterface {
         for (StateMapInterface map : maps) {
             map.getActiveNode().behaviour();
         }
-    }
-
-    public ArrayList<GameObjectInterface> getChildren() {
-        return children;
-    }
-
-    public GameObjectInterface getParent() {
-        return parent;
     }
 }
--- Coursework/src/main/java/DecisionEngine/GameObject/GameObjectInterface.java
+++ Coursework/src/main/java/DecisionEngine/GameObject/GameObjectInterface.java
@@ -4,6 +4,4 @@ import java.util.Collection;
 
 public interface GameObjectInterface extends Runnable {
     public Collection<StateMapInterface> getStateMaps();
-    public Collection<GameObjectInterface> getChildren();
-    public GameObjectInterface getParent();
 }

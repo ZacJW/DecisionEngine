5b0688cc446b2d56f292937b06df8d593d0b68d0
ZacJW <11651573+ZacJW@users.noreply.github.com>
2021-01-04 14:50:13 +0000

    Added initialise method to GameObjectInterface

--- Coursework/src/main/java/DecisionEngine/GameObject/GameObjectInterface.java
+++ Coursework/src/main/java/DecisionEngine/GameObject/GameObjectInterface.java
@@ -4,4 +4,11 @@ import java.util.Collection;
 
 public interface GameObjectInterface extends Runnable {
     public Collection<StateMapInterface> getStateMaps();
+
+    /**
+     * Initialises the object so it can be rendered.
+     * 
+     * All calls to OpenGL should be done here so it can be called by the main thread.
+     */
+    public void initialise();
 }

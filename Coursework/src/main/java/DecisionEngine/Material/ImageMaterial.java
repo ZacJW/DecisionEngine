669b8c41a0dd69ac0cd70208df99c2cb8ee9f79e
ZacJW <11651573+ZacJW@users.noreply.github.com>
2021-01-04 14:46:11 +0000

    Updated ImageMaterial to use transforms

--- Coursework/src/main/java/DecisionEngine/Material/ImageMaterial.java
+++ Coursework/src/main/java/DecisionEngine/Material/ImageMaterial.java
@@ -6,6 +6,8 @@ import DecisionEngine.Texture.ImageTexture;
 
 import static org.lwjgl.opengl.GL33C.*;
 
+import org.ejml.simple.SimpleMatrix;
+
 /**
  * A Material that uses Shader.imageShader and a single Texture
  */
@@ -20,10 +22,12 @@ public class ImageMaterial implements Material {
     }
 
     @Override
-    public void enable() {
+    public void enable(SimpleMatrix cameraTransform, SimpleMatrix globalTransform) {
         lwjgl.glUseProgram(shader.getShaderID());
         lwjgl.glActiveTexture(0);
         lwjgl.glUniform1i(lwjgl.glGetUniformLocation(shader.getShaderID(), "inTexture"), 0);
+        lwjgl.glUniformMatrix4fv(shader.getCameraTransformLocation(), false, cameraTransform.getFDRM().data);
+        lwjgl.glUniformMatrix4fv(shader.getGlobalTransformLocation(), false, globalTransform.getFDRM().data);
         lwjgl.glBindTexture(GL_TEXTURE_2D, texture.getTextureID());
     }
 

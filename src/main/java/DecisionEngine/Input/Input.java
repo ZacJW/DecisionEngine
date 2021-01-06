50499d6d446d6af9b511c188913d94eacf838894
ZacJW <11651573+ZacJW@users.noreply.github.com>
2021-01-06 13:57:17 +0000

    Finished Input

--- src/main/java/DecisionEngine/Input/Input.java
+++ src/main/java/DecisionEngine/Input/Input.java
@@ -1,21 +1,46 @@
 package DecisionEngine.Input;
 
-import org.lwjgl.glfw.GLFW;
+import java.util.ArrayList;
 
 import DecisionEngine.Core.EventCaptureInterface;
 import DecisionEngine.LWJGLDelegate.LWJGLInterface;
+import DecisionEngine.Listener.ListenPoint;
+import DecisionEngine.Utils.Point2D_Double;
 
 public class Input {
     LWJGLInterface lwjgl;
     long window;
     KeyInput keyInput;
     CursorPositionInput cursorPositionInput;
+    CursorButtonInput cursorButtonInput;
+
     public Input(EventCaptureInterface eventCapture, LWJGLInterface lwjgl, long window){
         this.lwjgl = lwjgl;
         this.window = window;
-        this.keyInput = new KeyInput(eventCapture);
-        this.cursorPositionInput = new CursorPositionInput();
-        GLFW.glfwSetKeyCallback(window, this.keyInput);
+        keyInput = new KeyInput(eventCapture);
+        cursorPositionInput = new CursorPositionInput(eventCapture);
+        cursorButtonInput = new CursorButtonInput(eventCapture, lwjgl);
+        lwjgl.glfwSetKeyCallback(window, keyInput);
+        lwjgl.glfwSetCursorPosCallback(window, cursorPositionInput);
+        lwjgl.glfwSetMouseButtonCallback(window, cursorButtonInput);
+    }
+
+    public ListenPoint<ArrayList<KeyAction>> getKeyListenPoint(int scancode){
+        return keyInput.getListenPoint(scancode);
+    }
+
+    public ListenPoint<ArrayList<Point2D_Double>> getCursorPositionListenPoint(){
+        return cursorPositionInput.getListenPoint();
+    }
+
+    public ListenPoint<ArrayList<CursorAction>> getCursorButtonListenPoint(int button){
+        return cursorButtonInput.getListenPoint(button);
+    }
+
+    public void processInput(){
+        keyInput.processKeyInput();
+        cursorPositionInput.processPositionInput();
+        cursorButtonInput.processButtonInput();
     }
 
 }

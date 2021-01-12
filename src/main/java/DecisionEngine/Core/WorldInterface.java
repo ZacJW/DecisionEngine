1b9ac96bc077b1b601e1c14ef78e500fb828492a
ZacJW <11651573+ZacJW@users.noreply.github.com>
2021-01-12 14:23:56 +0000

    Expanded WorldInterface
    WorldInterface now includes methods for updating a game object's position,
    requesting that a game object be spawned, and getting the input and
    renderer delegates.

--- src/main/java/DecisionEngine/Core/WorldInterface.java
+++ src/main/java/DecisionEngine/Core/WorldInterface.java
@@ -3,10 +3,20 @@ package DecisionEngine.Core;
 import org.ejml.simple.SimpleMatrix;
 
 import DecisionEngine.GameObject.GameObjectInterface;
+import DecisionEngine.Input.Input;
+import DecisionEngine.Render.RendererInterface;
 
 public interface WorldInterface {
 
+    public void updatePosition(GameObjectInterface gameObject, SimpleMatrix position);
+
     public SimpleMatrix getPosition(GameObjectInterface gameObject);
+
+    public void requestSpawn(GameObjectInterface gameObject, GameObjectInterface parent, SimpleMatrix position);
+
+    public Input getInput();
+
+    public RendererInterface getRenderer();
 
     public void mainloop();
 

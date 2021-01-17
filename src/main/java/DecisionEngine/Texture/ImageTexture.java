840718a1dc6e00e67fe2e06c139400ea697c4211
ZacJW <11651573+ZacJW@users.noreply.github.com>
2021-01-17 18:23:17 +0000

    Changed ImageTexture to use ByteBuffer rather than file path

--- src/main/java/DecisionEngine/Texture/ImageTexture.java
+++ src/main/java/DecisionEngine/Texture/ImageTexture.java
@@ -1,12 +1,7 @@
 package DecisionEngine.Texture;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
-import java.nio.channels.FileChannel;
 
 import static org.lwjgl.opengl.GL33C.*;
 import org.lwjgl.stb.*;
@@ -18,42 +13,38 @@ import DecisionEngine.LWJGLDelegate.LWJGLInterface;
 public class ImageTexture implements TextureInterface {
     int texture;
     LWJGLInterface lwjgl;
-    String path;
+    ByteBuffer rawImage;
     int min_filter;
     int mag_filter;
-    public ImageTexture(LWJGLInterface lwjgl, String path, int min_filter, int mag_filter){
+    boolean initialised = false;
+    public ImageTexture(LWJGLInterface lwjgl, ByteBuffer rawImage, int min_filter, int mag_filter){
         this.lwjgl = lwjgl;
-        this.path = path;
+        this.rawImage = rawImage;
         this.min_filter = min_filter;
         this.mag_filter = mag_filter;
     }
 
     @Override
     public void initialise() throws TextureInitialisationException {
-        try(FileInputStream imageFileStream = new FileInputStream(path)){
-            FileChannel imageFileChannel = imageFileStream.getChannel();
-        
-            ByteBuffer image;
-            try (MemoryStack stack = MemoryStack.stackPush()){
-                IntBuffer width = stack.callocInt(1);
-                IntBuffer height = stack.callocInt(1);
-                IntBuffer components = stack.callocInt(1);
-                STBImage.stbi_set_flip_vertically_on_load(true);
-                image = STBImage.stbi_load_from_memory(imageFileChannel.map(FileChannel.MapMode.READ_ONLY, 0, imageFileChannel.size()), width, height, components, 4);
-                texture = lwjgl.glGenTextures();
-                lwjgl.glBindTexture(GL_TEXTURE_2D, texture);
-                lwjgl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, min_filter);
-                lwjgl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mag_filter);
-                lwjgl.glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width.get(0), height.get(0), 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-                STBImage.stbi_image_free(image);
-            }
-
-            
-        }catch(FileNotFoundException e){
-            throw new TextureInitialisationException(e.toString());
-        }catch(IOException e){
-            throw new TextureInitialisationException(e.toString());
+        if (initialised){
+            return;
         }
+        ByteBuffer image;
+        try (MemoryStack stack = MemoryStack.stackPush()){
+            IntBuffer width = stack.callocInt(1);
+            IntBuffer height = stack.callocInt(1);
+            IntBuffer components = stack.callocInt(1);
+            STBImage.stbi_set_flip_vertically_on_load(true);
+            image = STBImage.stbi_load_from_memory(rawImage, width, height, components, 4);
+            rawImage = null;
+            texture = lwjgl.glGenTextures();
+            lwjgl.glBindTexture(GL_TEXTURE_2D, texture);
+            lwjgl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, min_filter);
+            lwjgl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mag_filter);
+            lwjgl.glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width.get(0), height.get(0), 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+            STBImage.stbi_image_free(image);
+        }
+        initialised = true;
     }
 
     public int getTextureID(){
